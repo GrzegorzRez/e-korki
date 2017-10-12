6be6c161ce549fac77a87761c95bcd6254a8aa87
SzagiMyMan <rezmergrzegorz@gmail.com>
2017-10-12 12:33:25 +0200

    dodano zdj

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,36 @@
 @extends('layouts/main')
 @section('content')
-cos
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img class="img-rounded img-responsive"  src="images/main_page_chalkboard_slider.jpg" alt="Tablica">
+    </div>
+
+    <div class="item">
+      <img class="img-rounded img-responsive" src="images/main_page_reading_book_slide.jpg" alt="Czyta">
+    </div>
+
+    <div class="item">
+      <img class="img-rounded img-responsive" src="images/main_page_slider_photo.jpg" alt="apple">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 @endsection
