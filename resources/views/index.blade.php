fe4a677de1c7d8bba328023b3f73441496f30857
Taran00 <michaltara1990@gmail.com>
2017-10-12 22:49:23 +0200

    dodany content na głównej
    Profity i te inne pierdoły. //fonty trza ściągnąć

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,18 +36,39 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+<h1 class="text-center">Jesteś nauczycielem?</h1>
 <div class="row">
   <div class="col-sm-4">
-  	<div class="row text-center"><img width="30%" src="images/cloud_arrow_up.svg"/> </div>
-  	<div class="row text-center">Udostępnianie materiałów dydaktycznych </div>
+  	<div class="row text-center"><img width="40%" src="images/heart-icon.svg"/> </div>
+  	<div class="row text-center"><p class="lead"><b>Stwórz własny profil, aby Uczniowie mogli kontaktować się z Tobą w sprawie korepetycji!</b></p></div>
   </div>
   <div class="col-sm-4">
-  	<div class="row text-center"><img width="30%" src="images/compass.svg"/> </div>
-  	<div class="row text-center">Mnóstwo ofert z całej polski</div>
+  	<div class="row text-center"><img width="40%" src="images/cloud_arrow_up.svg"/> </div>
+  	<div class="row text-center"><p class="lead"><b>Korzystaj z wbudowanego kalendarza, aby organizacja Twojej pracy była jeszcze wygodniejsza!</b></p> </div>
+  </div>
+  <div class="col-sm-4">
+  	<div class="row text-center"><img width="40%" src="images/compass.svg"/> </div>
+  	<div class="row text-center"><p class ="lead"><b>Udostępniaj pomoce dydaktyczne on-line, aby współpraca z Uczniem była jeszcze efektywniejsza!</b></p></div>
+  </div>
+
+ </div>
+ <h1 class="text-center">Jesteś uczniem?</h1>
+ <div class="row">
+  <div class="col-sm-4">
+  	
+  	<div class="row text-center"><img width="40%" src="images/compass.svg"/> </div>
+  	<div class="row text-center"><p class="lead"><b>Przeglądaj oferty korepetytorów z całej Polski i wybierz te, które najbardziej Ci odpowiadają!</b></p> </div>
   </div>
     <div class="col-sm-4">
-  	<div class="row text-center"><img width="30%" src="images/heart-icon.svg"/> </div>
-  	<div class="row text-center">Korzystaj z usług i oceniaj korepetytorów </div>
+  	<div class="row text-center"><img width="40%" src="images/cloud_arrow_up.svg"/> </div>
+  	<div class="row text-center"><p class="lead"><b>Używaj prostego i wygodnego czego(?), aby filtrować oferty według włąsnych preferencji! </b></div>
   </div>
+
+
+  <div class="col-sm-4">
+  	<div class="row text-center"><img width="40%" src="images/heart-icon.svg"/> </div>
+  	<div class="row text-center"><p class ="lead"><b>Po korepetycjach oceń Nauczyciela i prowadzone przez Niego zajęcia, aby dobór właściwych Nauczycieli był jeszcze dokładniejszy! </b></p></div>
+  </div>
+
 
 @endsection
