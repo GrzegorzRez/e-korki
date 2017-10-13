c6a2465c396e2371c2d8b2ca214663555d771596
Mateusz Kraska <mistrzmatik@gmail.com>
2017-10-13 12:27:28 +0200

    naprawa redirect bug

--- app/Http/Controllers/OpinionsController.php
+++ app/Http/Controllers/OpinionsController.php
@@ -6,6 +6,7 @@ use App\Opinion;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class OpinionsController extends Controller
 {
