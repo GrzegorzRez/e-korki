ffc762339e7e2b268dd8d5a74e680083fc52c47a
Mateusz Kraska <mistrzmatik@gmail.com>
2017-10-13 12:00:55 +0200

    poprawa wyświetlania opini

--- resources/views/opinions/index.blade.php
+++ resources/views/opinions/index.blade.php
@@ -4,12 +4,9 @@
 @foreach($opinions as $opinion)
     <div class="media">
         <div class="media-body">
-            <h4 class="media-heading">{{ $opinion->student->name }} <small><i>Wystawiono: {{ $opinion->created_at }}</i></small></h4>
+            <h4 class="media-heading">{{ $opinion->student->name }} {{ $opinion->student->surname }}<small><i>Wystawiono: {{ $opinion->created_at }}</i></small></h4>
             <p>{{ $opinion->content }}</p>
-            <p>
-                <span class="glyphicon glyphicon-star"></span>
-            </p>
-            <i class="glyphicon glyphicon-star"></i>
+            <p>Ocena: {{ $opinion->grade }}</p>
         </div>
     </div>
 @endforeach
