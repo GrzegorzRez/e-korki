82921f442110080a65d6e134005991b2859cdd7b
SzagiMyMan <rezmergrzegorz@gmail.com>
2017-10-13 10:42:38 +0200

    Dodano wyświetlanie ofert

--- resources/views/offer.blade.php
+++ resources/views/offer.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts/main')
 @section('content')
 	@foreach($offers as $offer)
+	<div class="list-group-item">
 		<h4>{{ $offer->user->name }} </h4>
 		<h3>{{ $offer->name }} </h3>
-
-
+		<h3>{{ $offer->price_per_hour }}</h3>
+		<h3>{{ $offer->description }}</h3>
+	</div>
 
 	@endforeach
 @endsection
