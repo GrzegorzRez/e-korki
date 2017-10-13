dd94bc0a0bc2f104008a320b86369921bc1e08c7
Mateusz Kraska <mistrzmatik@gmail.com>
2017-10-13 12:32:59 +0200

    naprawa web i profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -34,14 +34,8 @@
  					 <button class="btn btn-primary btn-lg">
                        <a href="{{route('profile.edit')}}"> Edytuj Profil</a>
                     </button>
-<<<<<<< HEAD
-                    @endif
 
-					<button class="btn btn-lg">
-						Wystaw opinię
-					</button>
-=======
-					@endif
+                    @endif
 
 					<a href="{{  route('opinions.add',['id'=>$user->id]) }}">
 						<button class="btn btn-lg">
@@ -49,7 +43,6 @@
 						</button>
 					</a>
 
->>>>>>> d8d7bbb2f0196d42acd7507da42b5cb62ce7a470
 			</div>
 
 
--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-<<<<<<< HEAD
-
-
-Route::get('/profile/edit','ProfileController@edit')->name('profile.edit');
-Route::get('/profile', 'ProfileController@index')->name('profile.index');
-Route::get('/profile/{id}', 'ProfileController@show')->name('profile.show');
-
-=======
 Route::get('/profil', 'ProfileController@index')->name('profile.index');
 Route::get('/profile/{id}', 'ProfileController@show')->name('profile.show');
 Route::get('/profil/edytuj', 'ProfileController@edit')->name('profile.edit');
 Route::post('/profile/store', 'ProfileController@store')->name('profile.store');
->>>>>>> d8d7bbb2f0196d42acd7507da42b5cb62ce7a470
+
 
 Route::get('/oferty','OfferController@index')->name('offers.index');
 Route::get('/oferty/dodaj', 'OfferController@add')->name('offers.add');
