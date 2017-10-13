d8ae1081d6bfc81e9102d87d760e1122167573dc
Mateusz Kraska <mistrzmatik@gmail.com>
2017-10-13 11:29:08 +0200

    clear rotes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,4 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', "MainController@index")->name('index');
 
 Auth::routes();
@@ -21,7 +9,6 @@ Route::get('/profile/{id}', 'ProfileController@profile')->name('profile');
 
 Route::get('/oferty','OfferController@index')->name('offers.index');
 Route::get('/oferty/dodaj', 'OfferController@add')->name('offers.add');
-Route::post('/oferty/store', 'OfferController@add')->name('offers.store');
+Route::post('/oferty/store', 'OfferController@store')->name('offers.store');
 
 Route::get('/opinie', 'OpinionsController@index')->name('opinions.index');
-
