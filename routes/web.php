c278e128b88354f176e422159c5e9eb5c47fab29
Taran00 <michaltara1990@gmail.com>
2017-10-13 09:51:32 +0200

    profil uzytkownika
    strona profilowa uzytkownika/prototyp :D

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/', "MainController@index")->name('index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/profile/{id}', 'ProfileController@profile')->name('profile');
+
