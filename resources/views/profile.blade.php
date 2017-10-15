d5838a6b59bcced436c7a891d47134da9a441d9e
SzagiMyMan <rezmergrzegorz@gmail.com>
2017-10-15 16:01:12 +0200

    Poprawiono ucinanie imion w opiniach

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -71,7 +71,7 @@
 		@foreach($opinions as $opinion)
 			<div class="media">
 				<div class="media-body">
-					<div class="row">
+					<div>
 						<h4 class="media-heading left">{{ $opinion->student->name }} {{ $opinion->student->surname }}<small class="right"><i>Wystawiono: {{ $opinion->created_at }}</i></small></h4>
 					</div>
 					<p>{{ $opinion->content }}</p>
