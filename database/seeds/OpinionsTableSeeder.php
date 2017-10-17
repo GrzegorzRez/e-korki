55314c48f7b3fffd0cc1f4889db765b5b1503b1b
Mateusz Kraska <mistrzmatik@gmail.com>
2017-10-17 14:21:20 +0200

    zaktualizowano seed opini do aktualego stany rzeczy

--- database/seeds/OpinionsTableSeeder.php
+++ database/seeds/OpinionsTableSeeder.php
@@ -19,13 +19,13 @@ class OpinionsTableSeeder extends Seeder
         $opinion->save();
 
         $faker = Faker\Factory::create();
-        for( $i=1 ; $i <= 99 ; $i++ ){
+        for( $i=1 ; $i <= 95 ; $i++ ){
             for( $j=1 ; $j <= 5 ; $j++ ) {
                 $opinion = new Opinion();
                 $opinion->teacher_id=$i;
-                $opinion->student_id=$i+1;
+                $opinion->student_id=$i+$j;
                 $opinion->content = $faker->sentence(10);
-                $opinion->grade = $faker->numberBetween(1, 10);
+                $opinion->grade = $faker->numberBetween(1, 6);
                 $opinion->save();
             }
         }
