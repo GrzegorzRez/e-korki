1120b7c9bd52d958df0c1419690f3a52b2c4c364
SzagiMyMan <rezmergrzegorz@gmail.com>
2017-10-18 19:42:44 +0200

    dodano pakiet pozwalający na logowanie przez fb
    Logowanie jeszcze nie działa

--- config/app.php
+++ config/app.php
@@ -143,6 +143,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Laravel\Socialite\SocialiteServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -195,6 +196,7 @@ return [
 
     'aliases' => [
 
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
