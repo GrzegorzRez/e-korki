88f797ab672a99f2fa96eb726b89afc01893bcfd
TomaszKench <mateusz1997bizuma@gmail.com>
2017-10-19 22:00:29 +0200

    idespac

---
+++ resources/views/offers/describe.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts/main')
+
+@section('content')
+
+<div class="container">
+    
+    <div class="col-lg-8">
+        
+        <h2>Nazwa Oferty</h2>
+        <p><h4>Miejscowość</h4></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    
+    <div class="col-lg-4 center-block">
+        <div class="row col-lg-12">
+        <h4>User</h4>
+        </div>
+        <div class="row col-lg-12">
+        <button type="button" class="btn btn-primary col-lg-8">Napisz wiadomość</button>                
+        </div>
+        <div class="row col-lg-12">
+        <button type="button" class="btn btn-primary col-lg-8">Weź numer telefonu</button>                    
+        </div> 
+    </div>    
+    
+</div>
+
+@endsection
