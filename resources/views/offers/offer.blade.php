c4b7fb6162c647844f05f558935ba993b6566896
Mateusz Kraska <mistrzmatik@gmail.com>
2017-10-19 09:08:50 +0200

    zmieniono wyglad podglady oferty

--- resources/views/offers/offer.blade.php
+++ resources/views/offers/offer.blade.php
@@ -1,9 +1,20 @@
-<div class="media opinion">
-    <div class="media-body">
-        <div>
-        	<h2>{{ $offer->name }}</h2>
+<div class="media opinion" onclick="window.location = '#' ">
+    <div class="media-body row">
+        <div class="col-xs-3">
+            <img src="{{ $offer->user->getAvatarHref()  }}"  class="img-rounded img-responsive">
         </div>
-        <p>{{ $offer->description }}</p>
-        <p>Cena: {{ $offer->price_per_hour }}</p>
+        <div class="col-xs-9">
+            <div class="row">
+                <div class="col-sm-10">
+                    <h2>{{ $offer->name }}</h2>
+                    <p>{{ $offer->user->getFullName() }}</p>
+                    <p>{{ $offer->description }}</p>
+                </div>
+                <div class="col-sm-2 text-right">
+                    <p>{{ $offer->category->name }}</p>
+                    <p>{{ $offer->price_per_hour }}/60 min.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
