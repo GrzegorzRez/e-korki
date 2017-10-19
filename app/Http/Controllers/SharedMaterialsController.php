d18ed75ff1393e53e8d85e596d176b2b133926e2
ja <mateusz1997bizuma@gmail.com>
2017-10-19 11:30:53 +0200

    widok dla materialow

--- app/Http/Controllers/SharedMaterialsController.php
+++ app/Http/Controllers/SharedMaterialsController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class SharedMaterialsController extends Controller
 {
-    //
+        public function index()
+    {
+    	return view('sharedMaterials');
+    }
 }
