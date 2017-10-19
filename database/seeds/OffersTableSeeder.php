8a358da542a70c96d124858163c8580488c2882f
Mateusz Kraska <mistrzmatik@gmail.com>
2017-10-19 08:53:54 +0200

    naparawa seeda ofert

--- database/seeds/OffersTableSeeder.php
+++ database/seeds/OffersTableSeeder.php
@@ -16,6 +16,7 @@ class OffersTableSeeder extends Seeder
         /*user_id price_per_hour name description*/
         $offer= new Offer();
         $offer->user_id=1;
+        $offer->category_id=1;
         $offer->price_per_hour=20;
         $offer->name='Matematyka';
         $offer->description='Opis oferty';
@@ -31,13 +32,14 @@ class OffersTableSeeder extends Seeder
         for( $i=1 ; $i <= 100 ; $i++ ){
             $offer= new Offer();
             $offer->user_id=$i;
+            $offer->category_id=2;
             $offer->price_per_hour=$faker->numberBetween(2,50);
             $offer->name=$faker->word;
             $offer->description=$faker->sentence(40);
             $offer->location=$locations[array_rand($locations)];
-            $offer->online=$faker->numberBetween(0,1);
-            $offer->teacher_home->$faker->numberBetween(0,1);
-            $offer->student_home->$faker->numberBetween(0,1);
+            $offer->online=$faker->boolean;
+            $offer->teacher_home=$faker->boolean;
+            $offer->student_home=$faker->boolean;
             $offer->save();
         }
     }
