38d310dc913af288410a5417d81dc4255ca6a4a9
ja <mateusz1997bizuma@gmail.com>
2017-10-19 11:08:18 +0200

    shared material controller

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -43,6 +43,7 @@
                         <ul class="dropdown-menu">
                             <li><a href="{{ route('profile.index')  }}">Profil</a></li>
                             <li><a href="#">Wiadomości</a></li>
+                            <li><a href="#">Udostępnione materiały</a></li>
                             <li><a href="#">Moje materiały</a></li>
                             <li class="divider"></li>
                             <li>
