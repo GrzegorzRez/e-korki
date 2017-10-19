5f405d656f6aa796249bcee1e2c21980f3aef870
Taran00 <michaltara1990@gmail.com>
2017-10-19 09:18:58 +0200

    dodano location/online
    jw

--- resources/views/offers/index.blade.php
+++ resources/views/offers/index.blade.php
@@ -3,7 +3,7 @@
 <div>
 	<form action="{{URL::current()}}">
 		<div>
-			Przedmiot: <input type="text" name="name" value="{{Input::get('name')}}">
+		Przedmiot: <input type="text" name="name" value="{{Input::get('name')}}">
 		<div>
 		<div>
 		<label for="">CENA</label>
@@ -11,12 +11,13 @@
 		Cena max: <input type="number" name="price_max" value="{{Input::get('price_max')}}">
 		<div>
 		<div>
-		Lokalizacja: <input type="text" name="location"> <!--chyba warto? -->
+		Lokalizacja: <input type="text" name="location" value="{{Input::get('location')}}"> <!--chyba warto? -->
 		<div>
 		<div>
 			<label for="">Tryb korepetycji</label>
 			Online<input type="checkbox" name="online">
-			Face-to-Face<input type="checkbox" name="face">
+			Miejsce zamieszkania nauczyciela<input type="checkbox" name="teacher-home">
+			Miejsce zamieszkania ucznia<input type="checkbox" name="student-home">
 		<div>
 		<input type="submit" value="wyszukaj">
 	</form>
@@ -29,7 +30,7 @@
 		<h3>{{ $offer->name }} </h3>
 		<h3>{{ $offer->price_per_hour }}</h3>
 		<h3>{{ $offer->description }}</h3>
-		<h4><b>Lokalizacja: </b>{{ $offer->user->location}}</h3>
+		<h4><b>Lokalizacja: </b>{{ $offer->location}}</h3>
 	</div>
 	@endforeach
 	{{ $offers->links() }}
