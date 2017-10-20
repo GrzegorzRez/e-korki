158d4f9be289ad1b2baec0bf31e3c7c3926b971f
Mateusz Kraska <mistrzmatik@gmail.com>
2017-10-20 12:02:07 +0200

    show view

--- resources/views/offers/show.blade.php
+++ resources/views/offers/show.blade.php
@@ -3,26 +3,45 @@
 @section('content')
 
 <div class="container">
-    
-    <div class="col-lg-8">
-        
-        <h2>Nazwa Oferty</h2>
-        <p><h4>Miejscowość</h4></p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    <div class="col-lg-8 col-md-8 col-sm-8">
+
+        <h2>{{ $offer->name }}</h2>
+        <p><h4>{{ $offer->location }}</h4></p>
+        <p>{{ $offer->description }}</p>
+        @if( $offer->online )
+            <span class="badge badge-pill badge-success">On-line</span>
+        @endif
+        @if( $offer->teacher_home )
+            <span class="badge badge-pill badge-success">U nauczyciela</span>
+        @endif
+        @if( $offer->student_home )
+            <span class="badge badge-pill badge-success">W domu ucznia</span>
+        @endif
     </div>
-    
-    <div class="col-lg-4 center-block">
-        <div class="row col-lg-12">
-        <h4>User</h4>
+
+    <div class="col-lg-4 col-md-4 col-sm-4 text-center">
+        <div class="row">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6">
+                <img src="{{ $offer->user->getAvatarHref() }}" class="img-rounded profile_picture img-responsive"/>
+            </div>
+            <div class="col-lg-3"></div>
         </div>
-        <div class="row col-lg-12">
-        <button type="button" class="btn btn-primary col-lg-8">Napisz wiadomość</button>                
+        <div class="row">
+            <h3>{{ $offer->user->getFullName() }}</h3>
+            <p><h5>{{ $offer->price_per_hour }} zł / 60 min</h5></p>
         </div>
-        <div class="row col-lg-12">
-        <button type="button" class="btn btn-primary col-lg-8">Weź numer telefonu</button>                    
-        </div> 
-    </div>    
-    
+        <div class="row">
+            <div class="btn-group-vertical">
+                <a href="{{ $offer->user->getProfileHref() }}" class="btn btn-primary" role="button">Zobacz profil</a>
+                <a href="#" class="btn btn-primary" role="button">Wyślij wiadomość</a>
+                <a href="#" class="btn btn-primary" role="button">Numer telefonu</a>
+
+            </div>
+        </div>
+    </div>
+
 </div>
 
 @endsection
