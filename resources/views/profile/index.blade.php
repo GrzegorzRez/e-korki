4fbccf327bea4cb003c5430ec8ad9836e54c55cb
Szagi <rezmergrzegorz@gmail.com>
2017-10-20 11:57:58 +0200

    commit nr telefonu

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -43,7 +43,7 @@
 					      </tr>
 					      <tr>
 					        <td><b>TELEFON:</b></td>
-					        <td>OPCJONALNIE</td> 
+					        <td>{{$user->phone}}</td> 
 					      </tr>
 					</table>
 				</div>
