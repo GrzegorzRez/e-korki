2b2b9142606fe6f2c7cb3811926ebf996b08c437
ja <mateusz1997bizuma@gmail.com>
2017-10-20 10:20:31 +0200

    uzupełnienie widoku oferty

--- resources/views/offers/show.blade.php
+++ resources/views/offers/show.blade.php
@@ -7,16 +7,39 @@
     <div class="col-lg-8 col-md-8 col-sm-8">
         
         <h2>{{ $offer->name }}</h2>
-        <p><h4>{{ $offer->location }}</h4></p>
+        <p><h4>{{ $offer->location }}</h4></p>   
         <p>{{ $offer->description }}</p>
+                    @if( $offer->online )
+                        <span class="badge badge-pill badge-success">On-line</span>
+                    @endif
+                    @if( $offer->teacher_home )
+                        <span class="badge badge-pill badge-success">U nauczyciela</span>
+                    @endif
+                    @if( $offer->student_home )
+                        <span class="badge badge-pill badge-success">W domu ucznia</span>
+                    @endif
     </div>
     
     <div class="col-lg-4 col-md-4 col-sm-4 text-center">
-        <h4>{{ $offer->user->getFullName() }}</h4>
+        <div class="row">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6">
+                <img src="{{ $offer->user->getAvatarHref() }}" class="img-rounded profile_picture img-responsive"/>
+            </div>
+            <div class="col-lg-3"></div>
+        </div>
+        <div class="row">
+        <h3>{{ $offer->user->getFullName() }}</h3>
+                <p><h5>{{ $offer->price_per_hour }} zł / 60 min</h5></p>
+        </div>
+        <div class="row">
         <div class="btn-group-vertical">
-         <button type="button" class="btn btn-primary">numer telefonu</button>
-         <button type="button" class="btn btn-primary">napisz wiadomosc</button>
-       </div> 
+            <a href="{{ $offer->user->getProfileHref() }}" class="btn btn-primary" role="button">Zobacz profil</a>
+            <a href="#" class="btn btn-primary" role="button">Wyślij wiadomość</a>
+            <a href="#" class="btn btn-primary" role="button">Numer telefonu</a>
+
+       </div>
+        </div>
     </div>    
     
 </div>
