6fda23e2940a7ea7e28780d702fb8e1be7b5e244
SzagiMyMan <32324697+SzagiMyMan@users.noreply.github.com>
2017-10-21 18:26:56 +0200

    dodano css do wiadomości

--- resources/views/messages/messages.blade.php
+++ resources/views/messages/messages.blade.php
@@ -1,15 +1,38 @@
 @extends('layouts.main')
 
 @section('content')
-	<div class="row">
-		<div class="review-block-content">
+<div class="container">
+	<div >
+		<div>
 			@foreach($messages as $message)
-				@if($message->send_id == Auth::id() && $message->receive_id == $user->id)	
-					<li><b>{{ $message->content }}</b></li>
+				@if($message->send_id == Auth::id() && $message->receive_id == $user->id)
+				<div class="row">
+					<div class="panel panel-primary col-xs-7">
+      					<div class="panel-heading ">
+      						{{Auth::user()->getFullName()}}
+      					</div>
+      					<div class="panel-body">
+      						{{ $message->content }}
+      					</div>
+      					<small>{{$message->created_at}}</small>
+    				</div>
+				</div>
 				@elseif($message->send_id == $user->id && $message->receive_id == Auth::id())
-					<li>{{ $message->content}}</li>
+				<div class="row">
+					<div class="col-xs-5"></div>
+					<div class="panel panel-danger col-xs-7">
+      					<div class="panel-heading text-right">
+      						{{$user->getFullName()}}
+      					</div>
+      					<div class="panel-body text-right">
+      						{{ $message->content }}
+      					</div>
+      					<small>{{$message->created_at}}</small>
+    				</div>
+    			</div>
 				@endif
 			@endforeach
 		</div> 
 	</div>
+</div>
 @endsection
