5d009486f1362156b6d1f4f96fb17b98b78b93b5
Mateusz Kraska <mistrzmatik@gmail.com>
2017-10-22 10:44:33 +0200

    rozszerzenie ilosci kategori i sortowanie ich

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -13,8 +13,10 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         Category::truncate();
-
-        $categories_names = ['Matematyka','j. polski','j. angielski','j. niemiecki','Programowanie','Chemia','Fizyka','Biologia'];
+        $categories_names = ['matematyka','j. polski','j. angielski','j. niemiecki','programowanie','chemia','fizyka','biologia', 'aktorstwo','budownictwo',
+            'ekonomia','filozofia','fotografia','geografia','grafika komputerowa','historia','informatyka','j. francuski','j. hiszpański','j. rosyjski',
+            'j. włoski','łacina','malarstwo','muzyka','nauczanie początkowe','wos'];
+        sort($categories_names);
         for( $i = 0 ; $i < count($categories_names) ; $i++ ){
             $category = new Category();
             $category->name = $categories_names[$i];
